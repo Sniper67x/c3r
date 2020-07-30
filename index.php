c606cbd1e6105db1f3f5a336bd57ae4a5efff7fc
Sniper67x <47265597+Sniper67x@users.noreply.github.com>
2020-07-30 16:48:36 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="shortcut icon" type="image/x-icon" href="https://cdn.discordapp.com/icons/700854924065374318/a_78fac69c4641bd08817f195c4026c11a.png?size=128"/>
+<style>
+body {
+  background-image: url('https://cdn.discordapp.com/attachments/701063977622437991/737792818688426000/0_20200729060100_1_edited.jpg');
+}
+</style>
+<head>
+<title>Code Three Response Player Count</title>
+</head>
+<body>
+    <img src=https://cdn.discordapp.com/icons/700854924065374318/a_78fac69c4641bd08817f195c4026c11a.png?size=128>
+ <div class="w3-display-middle w3-text-white">
+    <h1 class="w3-jumbo w3-animate-top">Players Currently Online:</h1>
+    <div class="w3-large w3-center">
+<?php
+   $response = file_get_contents("http://45.158.199.164:30124/dynamic.json");
+$json = json_decode($response);
+
+echo "<h2>{$json->clients}/{$json->sv_maxclients}</h2>";
+?>
+    </div>
+ </div>
+</body>
+</html>
